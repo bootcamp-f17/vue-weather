cc3e5923f8e00b18f7b68667142de70647ef4391
Gavin Cheng <gavincheng01@gmail.com>
2017-10-16 13:40:49 -0400

    got rid of bad red stuff

--- app/main.js
+++ app/main.js
@@ -5,31 +5,32 @@ var app = new Vue({
   data: {
 
     input: '',
-    output: '',
+    output: true,
     error: false,
-    errorMessage: 'https request could not be completed'
+    errorMessage: 'https request could not be completed',
+    apiRequest:'',
+    temperatureOutputK: '',
+    temperatureOutputF: '',
+    temperatureOutputC: '',
+    cityOutput: '',
+    conditionOutput:'',
 
 
   },
 
   methods: {
 
-    httpErrorMessage: function() {
-      if (badApi === 'true') {
-        this.error = true;
 
-      }
-    },
 
     getWeather: function() {
 
   var url="https://api.openweathermap.org/data/2.5/weather?zip=<zipcode>&us&appid=ef6a94dab254dc386b931af4d5ca58c7";
-  url = url.replace("<zipcode>", zipInput.value); 
+  url = url.replace("<zipcode>", this.input); 
 
   apiRequest = new XMLHttpRequest();
-  apiRequest.onload = catchResponse;
-  apiRequest.onerror = httpErrorMessage;
-  apiRequest.open('get', url, true);
+  apiRequest.onload = this.catchResponse;
+  apiRequest.onerror = this.httpErrorMessage;
+  apiRequest.open('get',url, true);
   apiRequest.send();
 
     },
@@ -42,11 +43,79 @@ var app = new Vue({
         this.error = false;
         this.output = true;
 
-        parseResponse();
+        this.parseResponse();
       }
+        else {
 
-  }
+    this.httpErrorMessage = JSON.parse(apiRequest.responseText).message;
+    this.error= true;
+    this.output = false;
+
+    }
+
+  },
+
+    parseResponse: function () {
+
+    var results = JSON.parse(apiRequest.responseText);
+
+    var tempK = Math.round(results.main.temp);
+    var tempF = Math.round(9/5 * (tempK - 273) + 32); // 9/5 (K - 273) + 32
+    var tempC = tempK - 273; // K - 273
+
+    this.temperatureOutputK = tempK + "&deg;";
+    this.temperatureOutputF = tempF + "&deg;";
+    this.temperatureOutputC = tempC + "&deg;";
+
+    this.cityOutput = results.name;
+
+    this.conditionOutput = results.weather[0].description;
+
+    // if (tempF > 92) {
+    //   weatherImage.src = "http://www.thelandofshadow.com/wp-content/uploads/2013/04/mordor_by_edli-d2yrha5.jpg";
+    // }
+    // else if (tempF > 85) {
+    //   weatherImage.src = "https://media.rbl.ms/image?u=%2Ffiles%2F2016%2F04%2F15%2F635963469764710781-1002509858_tumblr_n6vi59KPmO1tbh1dho1_400.gif&ho=https%3A%2F%2Faz616578.vo.msecnd.net&s=765&h=09e03017b9751456564b4ce2542f761a7bb5f8d551aaf061b251294fbd316e80&size=980x&c=3646041352";
+    // }
+    // else if (tempF > 75) {
+    //   weatherImage.src = "http://i0.kym-cdn.com/photos/images/original/001/093/677/752.png";
+    // }
+    // else if (tempF > 65) {
+    //   weatherImage.src = "http://ecotechpoolservice.com/wp-content/uploads/2012/10/Swimming-Pool-Sustainability.jpg";
+    // }
+    // else if (tempF > 32) {
+    //   weatherImage.src = "http://yumgoggle.com/wp-content/uploads/2015/12/Hot-Cocoa-DSC_4177-square-600.jpg";
+    // }
+    // else {
+    //   weatherImage.src = "http://clipartix.com/wp-content/uploads/2016/05/Free-weather-clipart-clip-art-pictures-graphics-illustrations-6.jpg";
+    // }
 
 }
+
+
+
+
+
+
+
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
