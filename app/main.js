f143588ef397d95dfbb0d33ec879dd5eee9efe89
trenshaw88 <trenshaw88@twc.com>
2017-10-16 14:24:55 -0400

    Initial Commit

--- app/main.js
+++ app/main.js
@@ -1,12 +1,16 @@
+const openWeatherBaseUrl = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&us&appid=ef6a94dab254dc386b931af4d5ca58c7"
+
 var app = new Vue({
 
   el: '#inputForm',
 
   data: {
 
-    input: '',
-    error: false
-
+    zipInput: '',
+    error: false,
+    results: [],
+    dataResults: []
   },
 
   methods: {
@@ -19,11 +23,35 @@ var app = new Vue({
 
     getWeather: function() {
 
-      var url = 'http://api.openweathermap.org/data/2.5/weather?zip=<zipcode>&us&appid=ef6a94dab254dc386b931af4d5ca58c7';
+      var url = openWeatherBaseUrl + this.zipInput + apiKey; 
 
-      alert("hello!");
+      console.log(this.zipInput);
+      console.log(url);
+
+      axios.get(url)
+      .then(response => this.results = response.data);
+      
+      console.log(JSON.parse(this.results);
+      //this.results = this.dataResults;
+
+
+
+      //console.log(this.dataResults);
+
+      //console.log(this.results);
+
+      //return this.parseResponse;
+
+      //alert("hello!");
 
     }
+
+    //parseResponse: function() {
+     // var results = JSON.parse(this.dataResults);
+      //console.log(this.results.name);
+
+    //}
+
 
   }
 
